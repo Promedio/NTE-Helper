a0887fa8201029815800d67503953d51bcc63fdb
Ecrop Roinuat <xperz@qq.com>
2026-06-10 02:36:02 +0800

    新增了构建后自动加壳的流程。

--- NTE Stumps/build.bat
+++ NTE Stumps/build.bat
@@ -9,15 +9,28 @@
 
 SET compiler_path=%PROGRAMFILES%\AutoHotkey\Compiler\Ahk2Exe.exe
 SET base_path=%PROGRAMFILES%\AutoHotkey\v2\AutoHotkey64.exe
+SET upx_path=%PROGRAMFILES%\AutoHotkey\Compiler\Upx.exe
 
 ECHO Compiler: %compiler_path%
 ECHO Base Bin: %base_path%
 ECHO Source: %~dp0src\main.ahk
 ECHO Output: %~dp0NTE Stumps.exe
 
-"%compiler_path%" /base "%base_path%" /in ".\src\main.ahk" /out ".\NTE Stumps.exe"
+"%compiler_path%" /base "%base_path%" /in ".\src\main.ahk" /out ".\NTE Stumps.exe" || GOTO EXIT
+
+IF EXIST "%upx_path%" (
+	ECHO Upx: %upx_path%
+	"%upx_path%" --ultra-brute --no-lzma ".\NTE Stumps.exe" || GOTO EXIT
+)
 
 
 
-PAUSE
+:EXIT
+IF ERRORLEVEL 1 (
+	ECHO Build failed, press any key to exit . . . 
+) ELSE (
+	ECHO Build complete, press any key to exit . . . 
+)
+
+PAUSE >NUL
 
